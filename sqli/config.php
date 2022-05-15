4a8bb5480b59f2d8fcedee2b3516fd0e769da34e
Lorenzo Castorina <lorenzo.castorina@icloud.com>
2022-05-15 16:53:22 +0200

    change config

--- sqli/config.php
+++ sqli/config.php
@@ -1,7 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost:3036');
-   define('DB_USERNAME', 'admin');
-   define('DB_PASSWORD', 'password');
-   define('DB_DATABASE', 'example_database');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'test');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
